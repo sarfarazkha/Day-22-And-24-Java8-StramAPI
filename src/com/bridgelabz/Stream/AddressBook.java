807e803934b70470de68f6104813970d7acf08a5
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-30 23:40:20 +0530

    Avoid Duplicate Contact

---
+++ src/com/bridgelabz/Stream/AddressBook.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.Stream;
+
+import java.util.Scanner;
+
+public class AddressBook{
+    private  String addressBookName;
+    static Scanner scanner = new Scanner(System.in);
+
+    public AddressBook(String addressBookName) {
+        this.addressBookName = addressBookName;
+    }
+
+    public AddressBook() {
+    }
+
+    void addAddressBook() {
+        System.out.println("Enter the name of AddressBook");
+        addressBookName = scanner.next();
+    }
+
+    public String getAddressBookName() {
+        return addressBookName;
+    }
+
+    public void setAddressBookName(String addressBookName) {
+        this.addressBookName = addressBookName;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "addressBookName='" + addressBookName + '\'' +
+                '}';
+    }
+}
---
+++ src/com/bridgelabz/Stream/AddressMain.java
@@ -0,0 +1,101 @@
+package com.bridgelabz.Stream;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class AddressMain {
+
+    static Map<String, Contacts> contactsMap = new HashMap<>();
+
+    static Map<String, AddressBook> addressBookMap = new HashMap<>();
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static void addNewContact() {
+        Contacts contacts = new Contacts();
+        contacts.addContact();
+        contactsMap.put(contacts.getPhoneNumber(), contacts);
+    }
+
+    private static void editContact() {
+        System.out.println("Enter the contactNumber");
+        String phoneNumber = scanner.next();
+        Contacts contacts = contactsMap.get(phoneNumber);
+        if (contacts != null) {
+            contacts.addContact();
+        } else {
+            System.out.println("The contact is not present in Addressbook");
+        }
+    }
+
+    private static void showContact() {
+        System.out.println("Enter the contactNumber");
+        String phoneNumber = scanner.next();
+        Contacts contacts = contactsMap.get(phoneNumber);
+        if (contacts != null) {
+            contacts.displayContact();
+        } else {
+            System.out.println("The contact is not present in AddressBook");
+        }
+    }
+
+    private static void deleteContact() {
+        System.out.println("Enter the ContactNumber");
+        String phoneNumber = scanner.next();
+        Contacts contacts = contactsMap.get(phoneNumber);
+        if (contacts != null) {
+            contactsMap.remove(contacts);
+        } else {
+            System.out.println("The contact is not present in Addressbook");
+        }
+    }
+    static void addNewAddressBook() {
+        AddressBook addressBook = new AddressBook();
+        addressBook.addAddressBook();
+        addressBookMap.put(addressBook.getAddressBookName(), addressBook);
+    }
+    static void contactOperations() {
+        int choice;
+        do {
+            System.out.println("Enter the choice");
+            System.out.println("1:ADD ADDRESSBOOK");
+            System.out.println("2:ADD CONTACT");
+            System.out.println("3:EDIT CONTACT");
+            System.out.println("4:DISPLAY CONTACT");
+            System.out.println("5:DELETE CONTACT");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addNewAddressBook();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    editContact();
+                    break;
+                case 4:
+                    showContact();
+                    break;
+                case 5:
+                    deleteContact();
+                default:
+                    System.out.println("Enter the choices between 1-5");
+                    contactOperations();
+            }
+        } while (choice != 5);
+    }
+    public static void main(String[] args) {
+
+        AddressMain addressMain= new AddressMain();
+        contactOperations();
+        AddressBook addressBook = new AddressBook();
+    }
+}
+
+
+
+
+
+
